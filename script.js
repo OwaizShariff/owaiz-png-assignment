57ea88b8bdbcbdcd0588b2e9b42e3b7bd82ec418
owaiz shariff <owaizs97@gmail.com>
2021-11-15 16:46:02 +0530

    pgn-assignment

---
+++ script.js
@@ -0,0 +1,21 @@
+var age=18;
+age >= 18 ? console.log("Adult") : console.log("Minor");
+
+if(age>=18){
+    console.log("Adult");
+}
+else{
+    console.log("Child");
+}
+
+var num=0;
+
+if(num>0){
+    console.log("Positive");
+}
+else if(num<0){
+    console.log("Negative");
+}
+else{
+    console.log("Given number is zero");
+}
